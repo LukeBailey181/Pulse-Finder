464cc6174075083f29f4e3fff75d29c65d6d83ed
Luke Bailey <lukebailey@college.harvard.edu>
2020-06-08 14:41:15 +0100

    added live face recognition from webcam input

---
+++ pulse-detection.py
@@ -0,0 +1,44 @@
+from imutils.video import VideoStream
+import face_recognition
+import argparse
+import imutils
+import pickle
+import time
+import cv2
+
+print("[INFO] starting video stream...")
+vs = VideoStream(src=0).start()
+writer = None
+time.sleep(2.0)
+
+while True:
+	# grab the frame from the threaded video stream
+	frame = vs.read()
+	
+	# convert the input frame from BGR to RGB then resize it to have
+	# a width of 750px (to speedup processing)
+	rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+	rgb = imutils.resize(frame, width=750)
+	r = frame.shape[1] / float(rgb.shape[1])
+	# detect the (x, y)-coordinates of the bounding boxes
+	# corresponding to each face in the input frame, then compute
+	# the facial embeddings for each face
+	boxes = face_recognition.face_locations(rgb,
+		model="hog")
+	encodings = face_recognition.face_encodings(rgb, boxes)
+	names = ["Face"]
+	for ((top, right, bottom, left), name) in zip(boxes, names):
+		# rescale the face coordinates
+		top = int(top * r)
+		right = int(right * r)
+		bottom = int(bottom * r)
+		left = int(left * r)
+		# draw the predicted face name on the image
+		cv2.rectangle(frame, (left, top), (right, bottom),
+			(0, 255, 0), 2)
+		y = top - 15 if top - 15 > 15 else top + 15
+		cv2.putText(frame, name, (left, y), cv2.FONT_HERSHEY_SIMPLEX,
+			0.75, (0, 255, 0), 2)
+		cv2.imshow("Frame", frame)
+		key = cv2.waitKey(1) & 0xFF
+
